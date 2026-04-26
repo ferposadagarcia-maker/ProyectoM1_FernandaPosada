1b495f7cfdeb71f8d72d79db8b495c284a761e99
Fernanda Posada <ferposadagarcia@gmail.com>
2026-04-26 00:27:11 -0600

    se agrego codigo hex junto con hsl

--- script.js
+++ script.js
@@ -12,6 +12,30 @@ function generarColorHSL() {
   return `hsl(${h}, ${s}%, ${l}%)`;
 }
 
+function hslAHex(color) {
+  const div = document.createElement("div");
+  div.style.color = color;
+
+  document.body.appendChild(div);
+
+  const rgb = getComputedStyle(div).color;
+
+  document.body.removeChild(div);
+
+  const valores = rgb.match(/\d+/g).map(Number);
+
+  const r = Number(valores[0]);
+  const g = Number(valores[1]);
+  const b = Number(valores[2]);
+
+  const rHex = r.toString(16).padStart(2, "0");
+  const gHex = g.toString(16).padStart(2, "0");
+  const bHex = b.toString(16).padStart(2, "0");
+
+  const hex = "#" + rHex + gHex + bHex;
+
+  return hex
+}
 function generarPaleta() {
   palette.innerHTML = "";
 
@@ -19,16 +43,20 @@ function generarPaleta() {
 
   for (let i = 0; i < cantidad; i++) {
     const color = generarColorHSL();
+    const hex = hslAHex(color);
 
     const l = parseInt(color.split(",")[2]);
     const textColor = l > 50 ? "black" : "white";
 
-    const div = document.createElement("div");
-    div.classList.add("color-box");
-    div.style.backgroundColor = color;
-    div.style.color = textColor;
-    div.textContent = color;
 
+    const div = document.createElement("div");
+        div.classList.add("color-box");
+        div.style.backgroundColor = color;
+        div.style.color = textColor;
+        div.textContent = color + "\n" + hex;
+        div.addEventListener("click", function() {
+         navigator.clipboard.writeText(color + " " + hex);
+        });
     palette.appendChild(div);
   }
 }
